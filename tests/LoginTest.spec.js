0300a8d95a5499b95e056b6a17882d729cd1d845
Yusuf-Hridoy <yusuf.cityu.cse@gmail.com>
2026-01-24 16:52:05 +0600

    testdata

--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -1,27 +1,28 @@
-const {test, expect} = require("@playwright/test");
-const env = require ('../utils/Testoption');
+const { test, expect } = require("@playwright/test");
+const env = require('../utils/Testoption');
 import { Loginpage } from "../Pages/Loginpage";
+const testData = require('../TestData/LoginData.json');
 
 // Remove page.close() - not needed with Playwright Test
-test('direct login', async ({page})=>{
+test('direct login', async ({ page }) => {
     const login = new Loginpage(page)
     await login.gotoLoginPage(env.base_url)
-    await login.LoginT1('standard_user' , 'secret_sauce')
+    await login.LoginT1(testData.validUser.username, testData.validUser.password)
     await expect(page).toHaveTitle('Swag Labs')
 })
 
-test('wrong password', async ({page})=>{
+test('wrong password', async ({ page }) => {
     const login = new Loginpage(page)
     await login.gotoLoginPage(env.base_url)
-    await login.LoginT1('standard_user' , 'wrong_password')
+    await login.LoginT1(testData.invalidPassword.username, testData.invalidPassword.password)
     const errorMessage = await login.getErrorMessage();
-    expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service')
+    expect(errorMessage).toBe(testData.invalidPassword.errorMessage)
 })
 
-test('wrong username', async ({page})=>{
+test('wrong username', async ({ page }) => {
     const login = new Loginpage(page)
     await login.gotoLoginPage(env.base_url)
-    await login.LoginT1('standard_use' , 'secret_sauce')
+    await login.LoginT1(testData.invalidUsername.username, testData.invalidUsername.password)
     const errorMessage = await login.getErrorMessage();
-    expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service')
+    expect(errorMessage).toBe(testData.invalidUsername.errorMessage)
 })
